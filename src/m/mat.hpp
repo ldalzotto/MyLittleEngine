a18226e56f0bc36ab56cb8b892d27f5476e661cc
ldalzotto <loic.dalzotto@hotmail.fr>
2022-05-03 12:38:20 +0200

    fix matrix mult

--- src/m/mat.hpp
+++ src/m/mat.hpp
@@ -122,17 +122,17 @@ static vec<T, 4> operator*(const mat<T, 4, 4> &p_left,
                            const vec<T, 4> &p_right) {
   vec<T, 4> l_return;
   l_return.at(0) =
-      (p_left.at(0, 0) * p_right.at(0)) + (p_left.at(0, 1) * p_right.at(1)) +
-      (p_left.at(0, 2) * p_right.at(2)) + (p_left.at(0, 3) * p_right.at(3));
+      (p_left.at(0, 0) * p_right.at(0)) + (p_left.at(1, 0) * p_right.at(1)) +
+      (p_left.at(2, 0) * p_right.at(2)) + (p_left.at(3, 0) * p_right.at(3));
   l_return.at(1) =
-      (p_left.at(1, 0) * p_right.at(0)) + (p_left.at(1, 1) * p_right.at(1)) +
-      (p_left.at(1, 2) * p_right.at(2)) + (p_left.at(1, 3) * p_right.at(3));
+      (p_left.at(0, 1) * p_right.at(0)) + (p_left.at(1, 1) * p_right.at(1)) +
+      (p_left.at(2, 1) * p_right.at(2)) + (p_left.at(3, 1) * p_right.at(3));
   l_return.at(2) =
-      (p_left.at(2, 0) * p_right.at(0)) + (p_left.at(2, 1) * p_right.at(1)) +
-      (p_left.at(2, 2) * p_right.at(2)) + (p_left.at(2, 3) * p_right.at(3));
+      (p_left.at(0, 2) * p_right.at(0)) + (p_left.at(1, 2) * p_right.at(1)) +
+      (p_left.at(2, 2) * p_right.at(2)) + (p_left.at(3, 2) * p_right.at(3));
   l_return.at(3) =
-      (p_left.at(3, 0) * p_right.at(0)) + (p_left.at(3, 1) * p_right.at(1)) +
-      (p_left.at(3, 2) * p_right.at(2)) + (p_left.at(3, 3) * p_right.at(3));
+      (p_left.at(0, 3) * p_right.at(0)) + (p_left.at(1, 3) * p_right.at(1)) +
+      (p_left.at(2, 3) * p_right.at(2)) + (p_left.at(3, 3) * p_right.at(3));
 
   return l_return;
 };
