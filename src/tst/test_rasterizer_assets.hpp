f9e0362a06a9445e1b9288324667d811ff8cc94c
ldalzotto <loic.dalzotto@hotmail.fr>
2022-05-10 09:19:12 +0200

    move test assets to specific files

---
+++ src/tst/test_rasterizer_assets.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <cor/container.hpp>
+
+// rast.single_triangle.visibility
+
+struct frame_expected {
+  static container::arr<ui8, 90> rast_single_triangle_visibility();
+  static container::arr<ui8, 107> rast_single_triangle_vertex_color_interpolation();
+  static container::arr<ui8, 92> rast_depth_comparison();
+  static container::arr<ui8, 8752> rast_depth_comparison_large_framebuffer();
+  static container::arr<ui8, 90> rast_depth_comparison_readonly();
+  static container::arr<ui8, 114> rast_depth_comparison_outofbounds();
+};
