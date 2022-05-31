10a3ae8e01c6079976e4346ffdc0bac481a3779e
ldalzotto <loic.dalzotto@hotmail.fr>
2022-05-31 20:42:44 +0200

    updating test data

--- src/tst/test_rasterizer_assets.hpp
+++ src/tst/test_rasterizer_assets.hpp
@@ -8,11 +8,11 @@ struct frame_expected {
   static container::arr<ui8, 90> rast_single_triangle_visibility();
   static container::arr<ui8, 107>
   rast_single_triangle_vertex_color_interpolation();
-  static container::arr<ui8, 99> rast_cull_clockwise();
+  static container::arr<ui8, 96> rast_cull_clockwise();
   static container::arr<ui8, 90> rast_cull_counterclockwise();
-  static container::arr<ui8, 92> rast_depth_comparison();
-  static container::arr<ui8, 8752> rast_depth_comparison_large_framebuffer();
-  static container::arr<ui8, 90> rast_depth_comparison_readonly();
-  static container::arr<ui8, 114> rast_depth_comparison_outofbounds();
-  static container::arr<ui8, 4243> rast_3Dcube();
+  static container::arr<ui8, 98> rast_depth_comparison();
+  static container::arr<ui8, 8597> rast_depth_comparison_large_framebuffer();
+  static container::arr<ui8, 97> rast_depth_comparison_readonly();
+  static container::arr<ui8, 118> rast_depth_comparison_outofbounds();
+  static container::arr<ui8, 6579> rast_3Dcube();
 };
