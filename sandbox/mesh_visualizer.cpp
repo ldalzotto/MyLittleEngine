1e0e314d6e9f359c1602479dbef970f7eb83b25c
ldalzotto <loic.dalzotto@hotmail.fr>
2022-06-12 19:49:16 +0200

    playing with mesh visualizer

--- sandbox/mesh_visualizer.cpp
+++ sandbox/mesh_visualizer.cpp
@@ -9,7 +9,11 @@ struct mesh_visualizer {
   bgfx::ProgramHandle m_frame_program;
 
   ren::camera_handle m_camera;
-  ren::mesh_handle m_mesh;
+  ren::mesh_handle m_mesh_0;
+  ren::mesh_handle m_mesh_1;
+
+  ren::mesh_handle *m_current_mesh;
+
   ren::shader_handle m_shader;
 
 private:
@@ -34,7 +38,8 @@ public:
 
     m_camera = p_engine.m_renderer.create_camera(l_camera);
 
-    auto l_obj_str = R""""(
+    {
+      auto l_obj_str = R""""(
 # Blender v2.76 (sub 0) OBJ File: ''
 # www.blender.org
 mtllib cube.mtl
@@ -70,11 +75,59 @@ f 3/3 4/4 7/7
 f 7/7 4/4 8/8
   )"""";
 
-    assets::mesh l_mesh = assets::obj_mesh_loader().compile(
-        container::range<ui8>::make((ui8 *)l_obj_str, std::strlen(l_obj_str)));
+      assets::mesh l_mesh =
+          assets::obj_mesh_loader().compile(container::range<ui8>::make(
+              (ui8 *)l_obj_str, std::strlen(l_obj_str)));
 
-    m_mesh = p_engine.m_renderer.create_mesh(l_mesh);
-    l_mesh.free();
+      m_mesh_0 = p_engine.m_renderer.create_mesh(l_mesh);
+      l_mesh.free();
+    }
+    {
+      auto l_obj_str = R""""(
+# Blender v2.76 (sub 0) OBJ File: ''
+# www.blender.org
+mtllib cube.mtl
+o Cube
+v -1.500000 1.000000 1.000000
+v 1.000000 1.000000 1.000000
+v -1.000000 -1.000000 1.000000
+v 1.000000 -1.000000 1.000000
+v -1.000000 1.000000 -1.000
+v 1.0000 1.000000 -1.00000
+v -1.000000 -1.000000 -1.000000
+v 1.000000 -1.000000 -1.000000
+vc 0 0 0
+vc 0 0 255
+vc 0 255 0
+vc 0 255 255
+vc 255 0 0
+vc 255 0 255
+vc 255 255 0
+vc 255 255 255
+o mat
+f 1/1 2/2 3/3
+f 2/2 4/4 3/3
+f 5/5 7/7 6/6
+f 6/6 7/7 8/8
+f 1/1 3/3 5/5
+f 5/5 3/3 7/7
+f 2/2 6/6 4/4
+f 6/6 8/8 4/4
+f 1/1 5/5 2/2
+f 5/5 6/6 2/2
+f 3/3 4/4 7/7
+f 7/7 4/4 8/8
+  )"""";
+
+      assets::mesh l_mesh =
+          assets::obj_mesh_loader().compile(container::range<ui8>::make(
+              (ui8 *)l_obj_str, std::strlen(l_obj_str)));
+
+      m_mesh_1 = p_engine.m_renderer.create_mesh(l_mesh);
+      l_mesh.free();
+    }
+
+    m_current_mesh = &m_mesh_0;
 
     m_shader = p_engine.m_renderer.create_shader(
         ColorInterpolationShader::s_vertex_output.range(),
@@ -84,26 +137,43 @@ f 7/7 4/4 8/8
   void free(eng::engine &p_engine) {
     p_engine.m_renderer.destroy(m_camera);
     p_engine.m_renderer.destroy(m_shader);
-    p_engine.m_renderer.destroy(m_mesh);
+    p_engine.m_renderer.destroy(m_mesh_0);
 
     bgfx::destroy(m_frame_program);
   };
 
-  ui32 m_counter = 0;
+  i32 m_counter = 0;
   fix32 m_delta = 0.1f;
 
   void frame(eng::engine &p_engine) {
+
+    {
+      eng::input::State *l_state;
+      p_engine.m_input_system.m_heap.m_state_table.at(
+          (uimax)eng::input::Key::ARROW_LEFT, &l_state);
+      if (*l_state == eng::input::State::JUST_PRESSED) {
+        m_current_mesh = &m_mesh_0;
+      }
+    }
+
+    {
+      eng::input::State *l_state;
+      p_engine.m_input_system.m_heap.m_state_table.at(
+          (uimax)eng::input::Key::ARROW_RIGHT, &l_state);
+      if (*l_state == eng::input::State::JUST_PRESSED) {
+        m_current_mesh = &m_mesh_1;
+      }
+    }
 
     m::mat<fix32, 4, 4> l_transform = m::mat<fix32, 4, 4>::getIdentity();
     l_transform =
         m::rotate_around(l_transform, fix32(m_counter) * m_delta, {0, 1, 0});
 
     container::arr<m::mat<fix32, 4, 4>, 1> l_mesh_transform = {l_transform};
-    container::arr<ren::mesh_handle, 1> l_meshes = {m_mesh};
+    container::arr<ren::mesh_handle, 1> l_meshes = {*m_current_mesh};
 
     p_engine.m_renderer.draw(m_camera, m_shader, l_mesh_transform.range(),
                              l_meshes.range());
-
     m_counter += 1;
   };
 
