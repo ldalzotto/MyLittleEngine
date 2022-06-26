5cab2ffc2470b9c947ccaf8902f4668389cbfeeb
ldalzotto <loic.dalzotto@hotmail.fr>
2022-06-26 16:11:11 +0200

    more rasterization tests migrated

---
+++ src/tst/test_engine_utils.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+#include <eng/engine.hpp>
--- src/tst/test_rasterizer_v2.cpp
+++ src/tst/test_rasterizer_v2.cpp
@@ -34,13 +34,14 @@ assert_frame_equals(const i8 *p_save_path, eng::engine_api<Engine> p_engine,
     }
 
 #if WRITE_OUTPUT
-    stbi_write_png(p_save_path, p_width, p_height, 3, (ui8*)l_frame_buffer_rgb.data(),
-                   3 * p_width);
+    stbi_write_png(p_save_path, p_width, p_height, 3,
+                   (ui8 *)l_frame_buffer_rgb.data(), 3 * p_width);
 #endif
 
     i32 l_length;
-    l_png_frame.m_begin = stbi_write_png_to_mem(
-        (ui8*)l_frame_buffer_rgb.data(), 3 * p_width, p_width, p_height, 3, &l_length);
+    l_png_frame.m_begin =
+        stbi_write_png_to_mem((ui8 *)l_frame_buffer_rgb.data(), 3 * p_width,
+                              p_width, p_height, 3, &l_length);
     l_png_frame.m_count = l_length;
 
     l_frame_buffer_rgb.free();
@@ -127,35 +128,16 @@ struct ColorInterpolationShader {
 
   template <typename Engine>
   inline static ren::shader_handle
-  load_shader(eng::engine_api<Engine> p_engine) {
-    return RasterizerTestToolbox::load_shader(p_engine, s_vertex_output.range(),
-                                              vertex, fragment);
+  load_shader(eng::engine_api<Engine> p_engine,
+              ren::shader_meta::cull_mode p_cull_mode =
+                  ren::shader_meta::cull_mode::clockwise) {
+    ren::shader_meta l_meta = l_meta.get_default();
+    if (l_meta.m_cull_mode != p_cull_mode) {
+      l_meta.m_cull_mode = p_cull_mode;
+    }
+    return RasterizerTestToolbox::load_shader(
+        p_engine, l_meta, s_vertex_output.range(), vertex, fragment);
   };
-};
-
-template <typename Rasterizer>
-inline static bgfx::ProgramHandle
-load_program(rast_api<Rasterizer> p_rast,
-             const container::range<rast::shader_vertex_output_parameter>
-                 &p_vertex_output,
-             rast::shader_vertex_function p_vertex,
-             rast::shader_fragment_function p_fragment,
-             bgfx::ShaderHandle *out_vertex, bgfx::ShaderHandle *out_fragment) {
-
-  uimax l_vertex_shader_size = rast::shader_vertex_bytes::byte_size(1);
-  const bgfx::Memory *l_vertex_shader_memory =
-      p_rast.alloc(l_vertex_shader_size);
-  rast::shader_vertex_bytes::view{l_vertex_shader_memory->data}.fill(
-      p_vertex_output, p_vertex);
-
-  const bgfx::Memory *l_fragment_shader_memory =
-      p_rast.alloc(rast::shader_fragment_bytes::byte_size());
-  rast::shader_fragment_bytes::view{l_fragment_shader_memory->data}.fill(
-      p_fragment);
-
-  *out_vertex = p_rast.createShader(l_vertex_shader_memory);
-  *out_fragment = p_rast.createShader(l_fragment_shader_memory);
-  return p_rast.createProgram(*out_vertex, *out_fragment);
 };
 
 }; // namespace RasterizerTestToolbox
@@ -179,9 +161,6 @@ TEST_CASE("rastV2.single_triangle.visibility") {
   l_camera_view.set_width_height(l_width, l_height);
   l_camera_view.set_render_width_height(l_width, l_height);
   l_camera_view.set_projection(m::mat<fix32, 4, 4>::getIdentity());
-  // l_camera_view.set_local_position({0,0,1});
-  // l_camera_view.set_local_rotation(m::rotate_around(m::pi<fix32>(), {0, 1,
-  // 0}));
 
   auto l_mesh_raw = container::arr_literal<ui8>(R""""(
 # Blender v2.76 (sub 0) OBJ File: ''
@@ -220,10 +199,153 @@ f 1 2 3
   l_scene.mesh_renderer_destroy(l_mesh_renderer);
   l_scene.camera_destroy(l_camera);
 
+  l_engine.renderer_api().destroy(l_mesh_handle, l_engine.rasterizer_api());
+  l_engine.renderer_api().destroy(l_shader_handle, l_engine.rasterizer_api());
+
+  l_scene.free();
+  __engine.free();
+}
+
+TEST_CASE("rastV2.single_triangle.vertex_color_interpolation") {
+  constexpr ui16 l_width = 8, l_height = 8;
+  using engine_t =
+      eng::details::engine<ren::details::ren_impl, rast_impl_software>;
+  engine_t __engine;
+  __engine.allocate(l_width, l_height);
+  eng::engine_api<engine_t> l_engine = {__engine};
+
+  using scene_t = eng::scene<engine_t>;
+  scene_t l_scene{&__engine};
+  l_scene.allocate();
+
+  eng::object_handle l_camera = l_scene.camera_create();
+  eng::camera_view<scene_t> l_camera_view = l_scene.camera(l_camera);
+  l_camera_view.set_width_height(l_width, l_height);
+  l_camera_view.set_render_width_height(l_width, l_height);
+  l_camera_view.set_projection(m::mat<fix32, 4, 4>::getIdentity());
+
+  auto l_mesh_raw = container::arr_literal<ui8>(R""""(
+# Blender v2.76 (sub 0) OBJ File: ''
+# www.blender.org
+mtllib cube.mtl
+o Cube
+v 0.0 0.0 0.0
+v 1.0 0.0 0.0
+v 0.0 1.0 0.0
+vc 0 0 0
+vc 0 255 0
+vc 255 255 0
+f 1/1 2/2 3/3
+  )"""");
+
+  auto l_mesh = assets::obj_mesh_loader{}.compile(l_mesh_raw.range());
+  ren::mesh_handle l_mesh_handle =
+      l_engine.renderer_api().create_mesh(l_mesh, l_engine.rasterizer_api());
+  l_mesh.free();
+  ren::shader_handle l_shader_handle =
+      RasterizerTestToolbox::ColorInterpolationShader::load_shader(
+          eng::engine_api<engine_t>{__engine},
+          ren::shader_meta::cull_mode::cclockwise);
+
+  eng::object_handle l_mesh_renderer = l_scene.mesh_renderer_create();
+  eng::mesh_renderer_view<scene_t> l_mesh_renderer_view =
+      l_scene.mesh_renderer(l_mesh_renderer);
+  l_mesh_renderer_view.set_program(l_shader_handle);
+  l_mesh_renderer_view.set_mesh(l_mesh_handle);
+
+  __engine.update([&]() { l_scene.update(); });
+
+  RasterizerTestToolbox::assert_frame_equals(
+      "/media/loic/SSD/SoftwareProjects/glm/"
+      "rast.single_triangle.vertex_color_interpolation.png",
+      eng::engine_api<engine_t>{__engine}, l_width, l_height,
+      frame_expected::rast_single_triangle_vertex_color_interpolation());
+
+  l_scene.mesh_renderer_destroy(l_mesh_renderer);
+  l_scene.camera_destroy(l_camera);
 
   l_engine.renderer_api().destroy(l_mesh_handle, l_engine.rasterizer_api());
   l_engine.renderer_api().destroy(l_shader_handle, l_engine.rasterizer_api());
-  // l_engine.rasterizer_api().destroy(l_)
+
+  l_scene.free();
+  __engine.free();
+}
+
+TEST_CASE("rastV2.cull.clockwise.counterclockwise") {
+
+  constexpr ui16 l_width = 8, l_height = 8;
+  using engine_t =
+      eng::details::engine<ren::details::ren_impl, rast_impl_software>;
+  engine_t __engine;
+  __engine.allocate(l_width, l_height);
+  eng::engine_api<engine_t> l_engine = {__engine};
+
+  using scene_t = eng::scene<engine_t>;
+  scene_t l_scene{&__engine};
+  l_scene.allocate();
+
+  eng::object_handle l_camera = l_scene.camera_create();
+  eng::camera_view<scene_t> l_camera_view = l_scene.camera(l_camera);
+  l_camera_view.set_width_height(l_width, l_height);
+  l_camera_view.set_render_width_height(l_width, l_height);
+  l_camera_view.set_projection(m::mat<fix32, 4, 4>::getIdentity());
+
+  auto l_mesh_raw = container::arr_literal<ui8>(R""""(
+# Blender v2.76 (sub 0) OBJ File: ''
+# www.blender.org
+mtllib cube.mtl
+o Cube
+v 0.0 0.0 0.0
+v 1.0 0.0 0.0
+v 0.0 1.0 0.0
+v 0.0 0.0 0.0
+v 0.0 -1.0 0.0
+v -1.0 0.0 0.0
+f 1 2 3
+f 4 5 6
+  )"""");
+
+  auto l_mesh = assets::obj_mesh_loader{}.compile(l_mesh_raw.range());
+  ren::mesh_handle l_mesh_handle =
+      l_engine.renderer_api().create_mesh(l_mesh, l_engine.rasterizer_api());
+  l_mesh.free();
+
+  ren::shader_handle l_shader_cc =
+      RasterizerTestToolbox::WhiteShader::load_shader(
+          l_engine, ren::shader_meta::cull_mode::cclockwise);
+  ren::shader_handle l_shader_c =
+      RasterizerTestToolbox::WhiteShader::load_shader(
+          l_engine, ren::shader_meta::cull_mode::clockwise);
+
+  eng::object_handle l_mesh_renderer = l_scene.mesh_renderer_create();
+  eng::mesh_renderer_view<scene_t> l_mesh_renderer_view =
+      l_scene.mesh_renderer(l_mesh_renderer);
+  l_mesh_renderer_view.set_program(l_shader_c);
+  l_mesh_renderer_view.set_mesh(l_mesh_handle);
+
+  __engine.update([&]() { l_scene.update(); });
+
+  RasterizerTestToolbox::assert_frame_equals(
+      "/media/loic/SSD/SoftwareProjects/glm/"
+      "rast.cull.clockwise.png",
+      l_engine, l_width, l_height, frame_expected::rast_cull_clockwise());
+
+  l_scene.mesh_renderer(l_mesh_renderer).set_program(l_shader_cc);
+
+  __engine.update([&]() { l_scene.update(); });
+
+  RasterizerTestToolbox::assert_frame_equals(
+      "/media/loic/SSD/SoftwareProjects/glm/"
+      "rast.cull.counterclockwise.png",
+      l_engine, l_width, l_height,
+      frame_expected::rast_cull_counterclockwise());
+
+  l_scene.mesh_renderer_destroy(l_mesh_renderer);
+  l_scene.camera_destroy(l_camera);
+
+  l_engine.renderer_api().destroy(l_mesh_handle, l_engine.rasterizer_api());
+  l_engine.renderer_api().destroy(l_shader_c, l_engine.rasterizer_api());
+  l_engine.renderer_api().destroy(l_shader_cc, l_engine.rasterizer_api());
 
   l_scene.free();
   __engine.free();
