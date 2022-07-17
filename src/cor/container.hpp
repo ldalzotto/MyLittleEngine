840f74076a9dea893a6c8b2ba5169be63f7548e9
ldalzotto <loic.dalzotto@hotmail.fr>
2022-07-17 16:22:08 +0200

    Moving container traits to bottom

--- src/cor/container.hpp
+++ src/cor/container.hpp
@@ -98,15 +98,6 @@ template <typename T> struct range {
   };
 };
 
-namespace traits {
-template <typename T> struct is_range {
-  inline static constexpr ui8 value = 0;
-};
-template <typename T> struct is_range<range<T>> {
-  inline static constexpr ui8 value = 1;
-};
-}; // namespace traits
-
 template <typename T, int N> struct arr {
   T m_data[N];
   container::range<T> range() { return container::range<T>::make(m_data, N); };
@@ -1368,5 +1359,14 @@ private:
         m_data, sizeof(*m_data) * m_intrusive.m_keys_intrisic.m_capacity);
   };
 };
+
+namespace traits {
+template <typename T> struct is_range {
+  inline static constexpr ui8 value = 0;
+};
+template <typename T> struct is_range<range<T>> {
+  inline static constexpr ui8 value = 1;
+};
+}; // namespace traits
 
 } // namespace container
